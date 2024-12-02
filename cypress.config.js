300485cc5d74f773d48023706e492f68bed52617
Faris Rahman <farisrhmn15@gmail.com>
2024-12-02 19:38:21 +0700

    Initial commit

---
+++ cypress.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  e2e: {
+    baseUrl: "http://your-application-url.com", // Ganti dengan URL aplikasi Anda
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+};
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,30 @@
+describe("Login Functionality", () => {
+  it("should log in successfully with valid credentials", () => {
+    cy.visit("http://192.168.56.1:8080/");
+
+    cy.get("#username").type("user");
+    cy.get("#password").type("password");
+
+    cy.get('button[type="submit"]').click();
+
+    cy.url().should("include", "/dashboard");
+    cy.get("h1").should("contain", "Welcome to Dashboard");
+  });
+
+  it("should show error for invalid credentials", () => {
+    cy.visit("http://192.168.56.1:8080/");
+
+    cy.get("#username").type("invalid-user");
+    cy.get("#password").type("invalid-password");
+
+    cy.get('button[type="submit"]').click();
+
+    cy.get("#errorMessage").should("be.visible");
+  });
+
+  it("should disable the login button if fields are empty", () => {
+    cy.visit("http://192.168.56.1:8080/");
+
+    cy.get('button[type="submit"]').should("be.disabled"); // Verifikasi tombol login disabled ketika form kosong
+  });
+});
