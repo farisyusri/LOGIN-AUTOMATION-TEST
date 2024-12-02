33206a577f946a73875c6096a281a6c2e5a7e666
Faris <farisrhmn15@gmail.com>
2024-12-02 23:25:32 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   e2e: {
-    baseUrl: "http://your-application-url.com", // Ganti dengan URL aplikasi Anda
+    baseUrl: "http://your-application-url.com",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
     },
   },
 };
